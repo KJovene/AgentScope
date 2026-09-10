1fb5b00ba9835e6ab4a8b24f99c8b52524e2ce27
MarcheschiThomas <marcheschi.thomas@gmail.com>
2026-09-10 15:55:32 +0200

    feat: Ajout de l'assistant IA

--- frontend/tests/setup.ts
+++ frontend/tests/setup.ts
@@ -80,6 +80,9 @@ function patchFetchSignal() {
 // jsdom doesn't implement scrollIntoView either; chat-style views call it to follow new messages.
 window.HTMLElement.prototype.scrollIntoView = () => {};
 
+// jsdom doesn't implement scrollIntoView either; chat-style views call it to follow new messages.
+window.HTMLElement.prototype.scrollIntoView = () => {};
+
 // The machine running CI-less coverage can be slow; give async utils room.
 configure({ asyncUtilTimeout: 3000 });
 
